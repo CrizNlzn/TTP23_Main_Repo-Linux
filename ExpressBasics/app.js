850d51c5a050aea6b8ee0ed968abf987f6469f9e
CrizNlzn <chrissgentile@gmail.com>
2023-07-11 13:50:08 -0400

    Express Lab

---
+++ ExpressBasics/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send(`
+      <html>
+        <head>
+          <title>Criz_site</title>
+        </head>
+        <body>
+          <h1>Hello Criz</h1>
+        </body>
+      </html>
+    `);
+});
+
+app.get("/criz", (req, res) => {
+res.send(`criz`);
+});
+
+const PORT = 1337;
+app.listen(PORT, () => {
+console.log(`listening on criz port ${PORT}`);
+});
